b003324381b5970f1e0b2b306ab1ee8dc2503e99
Junior Rodrigues <rodriguesx@proton.me>
2025-06-11 20:28:54 -0300

    Criando account request

---
+++ backend/src/main/java/com/voidbank/backend/controller/request/CreateAccountRequest.java
@@ -0,0 +1,17 @@
+package com.voidbank.backend.controller.request;
+
+import com.voidbank.backend.model.DocumentType;
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+public class CreateAccountRequest {
+
+    private String ownerName;
+    private String document;
+    private DocumentType documentType;
+
+}
