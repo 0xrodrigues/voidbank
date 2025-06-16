c027a8bbd60a92e30be54b5c9d838d4ecb707fa4
Junior Rodrigues <rodriguesx@proton.me>
2025-06-16 19:29:06 -0300

    criando enum para armazenar nome dos topicos

---
+++ backend/src/main/java/com/voidbank/backend/EventTopicEnum.java
@@ -0,0 +1,15 @@
+package com.voidbank.backend;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public enum EventTopicEnum {
+
+    TRANSACTION_PROCESS("voidbank.transaction.process.event", 1);
+
+    private final String topic;
+    private final Integer code;
+
+}
