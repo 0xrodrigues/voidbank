42394622cec61f2433f2dcba00da5b50dff8d32c
Junior Rodrigues <rodriguesx@proton.me>
2025-06-27 18:33:39 -0300

    Excluindo exceptions obsoletas

--- backend/src/main/java/com/voidbank/backend/exceptions/InsufficientBalanceException.java
+++
@@ -1,7 +0,0 @@
-package com.voidbank.backend.exceptions;
-
-public class InsufficientBalanceException extends RuntimeException {
-    public InsufficientBalanceException(String message) {
-        super(message);
-    }
-}
