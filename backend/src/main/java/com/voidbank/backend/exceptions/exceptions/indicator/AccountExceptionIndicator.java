75ce4d06a8182869feb0280e88d3c7f3fa5c4031
Junior Rodrigues <rodriguesx@proton.me>
2025-07-04 11:55:03 -0300

    alterando mensagem de erro de conta não encontrada

--- backend/src/main/java/com/voidbank/backend/exceptions/exceptions/indicator/AccountExceptionIndicator.java
+++ backend/src/main/java/com/voidbank/backend/exceptions/exceptions/indicator/AccountExceptionIndicator.java
@@ -11,7 +11,7 @@ import java.time.LocalDateTime;
 @AllArgsConstructor
 public enum AccountExceptionIndicator implements BusinessExceptionIndicator {
 
-    ACCOUNT_NOT_FOUND("ACCOUNT_NOT_FOUND", "Account with ID %s not found", HttpStatus.NOT_FOUND),
+    ACCOUNT_NOT_FOUND("ACCOUNT_NOT_FOUND", "Account specified not found", HttpStatus.NOT_FOUND),
     ACCOUNT_INSUFFICIENT_BALANCE("ACCOUNT_INSUFFICIENT_BALANCE", "Insufficient balance in the origin account", HttpStatus.BAD_REQUEST),
     ACCOUNT_WITH_DOCUMENT_ALREADY_EXISTS("ACCOUNT_WITH_DOCUMENT_ALREADY_EXISTS", "Unable to create account, document already registered", HttpStatus.BAD_REQUEST),
     RETRIEVING_BALANCE("RETRIEVING_BALANCE", "Unable to retrieve balance", HttpStatus.INTERNAL_SERVER_ERROR);
