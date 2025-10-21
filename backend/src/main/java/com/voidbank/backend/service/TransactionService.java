66425177bd63c46660b41f95f89ac25a13185feb
Junior Rodrigues <rodriguesx@proton.me>
2025-10-21 10:40:03 -0300

    chamando validate pipeline dentro do service

--- backend/src/main/java/com/voidbank/backend/service/TransactionService.java
+++ backend/src/main/java/com/voidbank/backend/service/TransactionService.java
@@ -1,19 +1,15 @@
 package com.voidbank.backend.service;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.voidbank.backend.model.Transaction;
 import com.voidbank.backend.model.TransactionEvent;
-import com.voidbank.backend.model.TransactionFailedValidationEvent;
 import com.voidbank.backend.model.enums.EventTopicEnum;
 import com.voidbank.backend.publisher.KafkaPublisher;
 import com.voidbank.backend.repository.TransactionRepository;
-import com.voidbank.backend.validator.TransactionValidator;
+import com.voidbank.backend.validator.TransactionValidatePipeline;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
-
-import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -21,16 +17,15 @@ import java.util.List;
 public class TransactionService {
 
     private static final String TRANSACTION_PROCESS_TOPIC = EventTopicEnum.TRANSACTION_PROCESS.getTopic();
-    private static final String TRANSACTION_FAILED_VALIDATION_TOPIC = EventTopicEnum.TRANSACTION_FAILED_VALIDATION.getTopic();
 
     private final TransactionRepository transactionRepository;
     private final KafkaPublisher publisher;
     private final ObjectMapper objectMapper;
-    private final List<TransactionValidator> validators;
+    private final TransactionValidatePipeline transactionValidatePipeline;
 
     public void transferFunds(Transaction transaction) {
         log.info("transferring funds and creating transaction - {}", transaction);
-        validateTransaction(transaction);
+        transactionValidatePipeline.validate(transaction);
         transaction.fill();
         transactionRepository.insert(transaction);
 
@@ -41,23 +36,4 @@ public class TransactionService {
             log.error("Error when producer event to {} topic", TRANSACTION_PROCESS_TOPIC, ex);
         }
     }
-
-    private void validateTransaction(Transaction transaction) {
-        try {
-            validators.forEach(v -> v.validate(transaction));
-        } catch (Exception ex) {
-            log.error("Transaction validation failed - {}", transaction);
-
-            try {
-                log.info("Producer transaction validation failed event to kafka - {}", transaction);
-                String exMessage = ex.getMessage();
-                String eventAsString = objectMapper.writeValueAsString(
-                        new TransactionFailedValidationEvent(transaction.getFrom(), transaction.getTo(), exMessage));
-                publisher.send(TRANSACTION_FAILED_VALIDATION_TOPIC, eventAsString);
-            } catch (JsonProcessingException jsonProcessingException) {
-                log.error("Error when producer event to {} topic", TRANSACTION_FAILED_VALIDATION_TOPIC, ex);
-            }
-        }
-    }
-
 }
